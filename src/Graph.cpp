02c07be696af08b37aec6b160944fa6cde080a7f
Jiajie Chen <jiegec@qq.com>
2018-05-17 16:01:41 +0800

    Add GPLv3 License

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -1,5 +1,24 @@
+// Copyright (C) 2018 Jiajie Chen
+// 
+// This file is part of OnePassSynthesisDMFB.
+// 
+// OnePassSynthesisDMFB is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// OnePassSynthesisDMFB is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with OnePassSynthesisDMFB.  If not, see <http://www.gnu.org/licenses/>.
+// 
+
 #include "Graph.h"
 #include <fstream>
+#include <exception>
 #include <string.h>
 
 using namespace std;
@@ -60,6 +79,7 @@ Graph::Graph(const char *file) {
       } else if (type == "DETECT") {
         node.type = DETECT;
         // unfinished
+        throw logic_error("Not supported graph node type");
       }
       this->nodes.emplace_back(move(node));
     }
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,21 @@
+// Copyright (C) 2018 Jiajie Chen
+// 
+// This file is part of OnePassSynthesisDMFB.
+// 
+// OnePassSynthesisDMFB is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// OnePassSynthesisDMFB is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with OnePassSynthesisDMFB.  If not, see <http://www.gnu.org/licenses/>.
+// 
+
 #include <iostream>
 #include <z3++.h>
 #include <chrono>
@@ -28,7 +46,7 @@ bool try_steps(const Graph &graph, int n) {
 }
 
 int main(int argc, char **argv) {
-  const char *filename = "../../testcase/Single_2_Input_Mix.txt";
+  const char *filename = "../../testcase/Assays/Testing/Single_2_Input_Mix.txt";
   if (argc > 1) {
     filename = argv[1];
   }
