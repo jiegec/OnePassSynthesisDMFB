d1e0fc973d4fca4a2cbe9eace43b870c1c0d79aa
Jiajie Chen <jiegec@qq.com>
2018-05-21 22:23:33 +0800

    Update readme

--- doc/auto/readme.el
+++ doc/auto/readme.el
@@ -6,12 +6,12 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("minted" "newfloat") ("eucal" "mathscr") ("placeins" "section")))
    (add-to-list 'LaTeX-verbatim-environments-local "minted")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -38,9 +38,10 @@
     "float"
     "svg")
    (LaTeX-add-labels
-    "sec:orgd637b02"
-    "sec:org56536c1"
-    "sec:org7afc7e1"
-    "sec:orgc5999f0"))
+    "sec:org7890733"
+    "sec:org866e4be"
+    "sec:org4e918c1"
+    "sec:orgc29ee50"
+    "sec:org895ad27"))
  :latex)
 
